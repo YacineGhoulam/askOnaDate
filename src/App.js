565a38af8b4f9eaf9650f26e798f709226a898d2
yacineghoulam <yacineghoulam@gmail.com>
2022-11-11 15:09:25 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [top, setTop] = useState(300);
+	const [left, setLeft] = useState(900);
+	const [yes, setYes] = useState(false);
+
+	const btnStyle = {
+		position: "absolute",
+		top: `${top}px`,
+		left: `${left}px`,
+	};
+
+	return (
+		<div className="App">
+			{!yes ? (
+				<div>
+					<h1>Will you go on a date with me?</h1>
+					<button
+						className="btn btn-green"
+						onClick={() => setYes(true)}
+					>
+						Yes
+					</button>
+					<button
+						className="btn btn-red"
+						style={btnStyle}
+						onMouseEnter={() => {
+							setTop(
+								Math.floor(
+									Math.random() * 500
+								) + 1
+							);
+							setLeft(
+								Math.floor(
+									Math.random() * 1400
+								) + 1
+							);
+						}}
+					>
+						No
+					</button>
+				</div>
+			) : (
+				<div>
+					<h1>yaay</h1>
+					<img src="thirsty-cat.png" />
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default App;
